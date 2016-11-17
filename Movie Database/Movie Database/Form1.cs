7a7fd8a9ed930fe54c89a4b755cfdd0a93ef35ef
noahgrostefon <n0ah1897@gmail.com>
2016-11-17 11:44:15 -0500

    All the Visual Studio Files
    All the files from the visual studio project folder

---
+++ Movie Database/Movie Database/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movie_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDirector.Clear();
+            txtGenre.Clear();
+            txtMovie.Clear();
+            txtProducer.Clear();
+            txtRelease.Clear();
+            cboAwards.SelectedIndex = -1;
+            cboSite.SelectedIndex = -1;
+        }
+    }
+}
